319916709505eeb60b3a8f66494d2e45f2804614
jiyang <jiyang@rubiconproject.com>
2018-01-08 23:53:13 -0800

    basic CRUD

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,17 @@
 const Koa = require('koa');
+const BodyParser = require('koa-bodyparser');
+
 const app = new Koa();
+const PORT = process.env.PORT || 1337;
 const indexRouters = require('./routers/index');
+const movieRouters = require('./routers/movies');
 
+app.use(BodyParser());
 app.use(indexRouters.routes());
+app.use(movieRouters.routes());
 
-module.exports = app.listen(3000);
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = server;
